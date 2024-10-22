21313a635dbee8b2cafeea07e9f8c29839685c3d
sabbene <11637137+sabbene@users.noreply.github.com>
2024-10-22 16:28:19 -0700

    update header

--- UnixBench/src/crud.c
+++ UnixBench/src/crud.c
@@ -1,26 +1,20 @@
 /*******************************************************************************
  *  The BYTE UNIX Benchmarks - Release 3
- *          Module: syscall.c   SID: 3.3 5/15/91 19:30:21
+ *          Module: crud.c   SID: 3.3 5/15/91 19:30:21
  *
  *******************************************************************************
  * Bug reports, patches, comments, suggestions should be sent to:
  *
- *	Ben Smith, Rick Grehan or Tom Yager at BYTE Magazine
- *	ben@bytepb.byte.com   rick_g@bytepb.byte.com   tyager@bytepb.byte.com
- *
  *******************************************************************************
  *  Modification Log:
- *  $Header: syscall.c,v 3.4 87/06/22 14:32:54 kjmcdonell Beta $
- *  August 29, 1990 - Modified timing routines
- *  October 22, 1997 - code cleanup to remove ANSI C compiler warnings
- *                     Andy Kahn <kahn@zk3.dec.com>
+ *  $Header: crud.c,v 0.1 24/10/22 17:00:00 $
  *
  ******************************************************************************/
 /*
- *  syscall  -- sit in a loop calling the system
+ *  crud  -- sit in a tight loop create/read/update/destroy file attempting to bypass caches
  *
  */
-char SCCSid[] = "@(#) @(#)syscall.c:3.3 -- 5/15/91 19:30:21";
+char SCCSid[] = "@(#) @(#)crud.c:0.1 -- 10/22/24 17:00:00";
 
 #define _GNU_SOURCE
 

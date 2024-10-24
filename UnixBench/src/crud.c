43837f5bcb2774afa788f97663dbff715ee141ae
sabbene <11637137+sabbene@users.noreply.github.com>
2024-10-24 16:46:50 -0700

    done use O_APPEND becuase it can conflict with O_DIRECT in some file systems

--- UnixBench/src/crud.c
+++ UnixBench/src/crud.c
@@ -33,24 +33,32 @@ char SCCSid[] = "@(#) @(#)crud.c:0.1 -- 10/22/24 17:00:00";
 
 unsigned long iter;
 char filename[20];
+int fd;
 
-void report()
+void cleanup()
 {
+    close(fd);
     if (access(filename, F_OK) == 0) {
         if (remove(filename) != 0) {
             perror("remove");
         }
     }
 
+}
+
+void report()
+{
+
+    cleanup();
 	fprintf(stderr,"COUNT|%ld|1|lps\n", iter);
 	exit(0);
 }
+
 
 int main(argc, argv)
 int	argc;
 char	*argv[];
 {
-    int fd;
 	int	duration;
     char *write_data;
     char *read_data;
@@ -74,23 +82,26 @@ char	*argv[];
         }
         if ( close(fd) < 0 ) {
             perror("close failed:");
+            cleanup();
             exit(1);
         }
         // write file
         fd = open(filename, O_WRONLY | O_DIRECT | O_SYNC, 0666);
         if ( fd == -1 ) {
             perror("open write only failed:");
+            cleanup();
             exit(1);
         }
 
         if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
             perror("clock_gettime:");
+            cleanup();
             exit(1);
         }
 
         if (posix_memalign((void **)&write_data, ALIGNMENT, ALIGNMENT) != 0) {
-            close(fd);
             perror("posid_memalign failed:");
+            cleanup();
             exit(1);
         }
 
@@ -98,16 +109,22 @@ char	*argv[];
         write_data[ALIGNMENT - 1] = '\0';
         bytes_written = write(fd, write_data,  ALIGNMENT);
         if (bytes_written == -1) {
-            close(fd);
-            free(write_data);
+            if ( write_data != NULL ) {
+                free(write_data);
+            }
+
             perror("write failed:");
+            cleanup();
             exit(1);
         }
 
-        free(write_data);
+        if (write_data != NULL) {
+            free(write_data);
+        }
 
         if ( close(fd) < 0 ) {
             perror("close failed:");
+            cleanup();
             exit(1);
         }
 
@@ -115,40 +132,65 @@ char	*argv[];
         fd = open(filename, O_RDONLY | O_DIRECT | O_SYNC, 0666);
         if ( fd == -1 ) {
             perror("open read only failed:");
+            cleanup();
             exit(1);
         }
 
         if (posix_memalign((void **)&read_data, ALIGNMENT, ALIGNMENT) != 0) {
-            close(fd);
-            perror("posid_memalign failed:");
+            perror("posix_memalign failed:");
+            cleanup();
             exit(1);
         }
 
-		lseek(fd, 0, SEEK_SET); // Reset file offset to the beginning
+        // Reset file offset to the beginning
+		if ( lseek(fd, 0, SEEK_SET) == -1 ) {
+            perror("Could not lseek to beginning of file:");
+            cleanup();
+            exit(1);
+        }
+
+        // read file
 		ssize_t bytes_read = read(fd, read_data, ALIGNMENT);
 		if (bytes_read == -1) {
-            free(read_data);
-			close(fd);
+            if ( read_data != NULL ) {
+                free(read_data);
+            }
 			perror("read file failed:");
-			exit(1);
+            cleanup();
+            exit(1);
 		}
 
-        free(read_data);
+        if (read_data != NULL ) {
+            free(read_data);
+        }
 
         if ( close(fd) < 0 ) {
             perror("close failed:");
+            cleanup();
             exit(1);
         }
 
+
 		// update file
-        fd = open(filename, O_WRONLY | O_APPEND | O_DIRECT | O_SYNC, 0666);
+        fd = open(filename, O_WRONLY | O_DIRECT | O_SYNC, 0666);
         if ( fd == -1 ) {
-            perror("open  append only failed:");
+            perror("open for append only failed:");
+            cleanup();
+            exit(1);
+        }
+
+        // seek to end of file
+        // sometimes O_APPEND isn't supported with O_DIRECT
+        // so we explicitly seek to the end of the file
+        if (lseek(fd, 0, SEEK_END) == -1) {
+            perror("Could not lseek to end of file:");
+            cleanup();
             exit(1);
         }
 
         if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
             perror("clock_gettime:");
+            cleanup();
             exit(1);
         }
 
@@ -156,20 +198,22 @@ char	*argv[];
         write_data[ALIGNMENT - 1] = '\0';
         bytes_written = write(fd, write_data,  ALIGNMENT);
         if (bytes_written == -1) {
-            close(fd);
             perror("append write failed:");
+            cleanup();
             exit(1);
         }
 
         if ( close(fd) < 0 ) {
             perror("close failed:");
+            cleanup();
             exit(1);
         }
 
 		// delete file
 		if (remove(filename) != 0) {
 			perror("delete failed");
-			exit(1);
+            cleanup();
+            exit(1);
 		}
 
         iter++;
